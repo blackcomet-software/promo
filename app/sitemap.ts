b97a23819d2e78151c9500da2dba126dee826d9f
nordinvandijk <nordinvandijk@icloud.com>
2024-11-10 20:24:42 +0100

    seo metadata code

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://blackcomet.net",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
