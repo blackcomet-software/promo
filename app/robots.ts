48d382773ea26fe80a4d288e5bd68f1e41b4d5e0
nordinvandijk <nordinvandijk@icloud.com>
2024-11-10 22:09:59 +0100

    fix: use www subdomain inside seo metadata

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://blackcomet.net/sitemap.xml",
+    sitemap: "https://www.blackcomet.net/sitemap.xml",
   };
 }
