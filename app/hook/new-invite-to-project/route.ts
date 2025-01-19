b4544cee7fa6e8b67048a87b50f11815e3ba9a8a
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-19 23:03:57 +0100

    feat: send in-app notification to user when invited to project

--- app/hook/new-invite-to-project/route.ts
+++ app/hook/new-invite-to-project/route.ts
@@ -1,8 +1,27 @@
+import { db } from "@/lib/drizzle"
+import { notification, usersInAuth } from "@/lib/drizzle/generated/schema"
+import { Tables } from "@/types/database.types"
+import { eq } from "drizzle-orm"
 import { NextResponse } from "next/server"
 
+type WebhookData = {
+  type: "INSERT",
+  table: "invite_to_project",
+  record: Tables<"invite_to_project">,
+  schema: "public",
+  old_record: null
+}
+
 export async function POST(req: Request) {
-  const data = await req.json()
-  console.log(data)
-  console.log("Webhook called this!")
+  const data = await req.json() as WebhookData
+
+  const query = await db.select().from(usersInAuth).where(eq(usersInAuth.email, data.record.target_email)).limit(1)
+  if (query.length === 0) {
+    return NextResponse.json({status: "success"})
+  } 
+
+  const user = query[0];
+  await db.insert(notification).values({ targetUserId: user.id, message: "You have been invited to a project!"})
+
   return NextResponse.json({status: "success"})
 }
