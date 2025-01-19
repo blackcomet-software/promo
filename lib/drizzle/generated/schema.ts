f8d5a6f0a7899dbbc8fa03008eff41e4f905b5a5
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-19 22:12:04 +0100

    Update schema.ts

--- lib/drizzle/generated/schema.ts
+++ lib/drizzle/generated/schema.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { pgTable, foreignKey, pgPolicy, uuid, timestamp, text, pgEnum } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 
