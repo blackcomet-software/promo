8f031d8abfa67d3d76128c1ed60778022131f337
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-19 22:26:11 +0100

    feat: log webhook data

--- app/hook/new-invite-to-project/route.ts
+++ app/hook/new-invite-to-project/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server"
 
-export async function POST() {
+export async function POST(req: Request) {
+  const data = await req.json()
+  console.log(data)
   console.log("Webhook called this!")
   return NextResponse.json({status: "success"})
 }
