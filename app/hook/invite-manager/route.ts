4cd2742ca3742bd9a0f4997052bdd58d0251e1e6
Nordin van Dijk <nordinvandijk@icloud.com>
2025-01-20 17:42:32 +0100

    fix: remove unused and exposing route

--- app/hook/invite-manager/route.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/drizzle";
-import { inviteToProject } from "@/lib/drizzle/generated/schema";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const allInvites = await db.select().from(inviteToProject)
-  console.log(allInvites)
- return NextResponse.json(allInvites) 
-}
